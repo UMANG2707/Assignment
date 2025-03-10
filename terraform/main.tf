c8944f4908f6377279ff284654bb48b643d27940
Umang <48994342+UMANG2707@users.noreply.github.com>
2025-03-11 00:59:20 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,21 +6,58 @@ resource "aws_security_group" "mysql_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["IP/32"]
+    cidr_blocks = ["103.137.19.231/32"]
   }
 
   ingress {
     from_port   = 3307
     to_port     = 3307
     protocol    = "tcp"
-    cidr_blocks = ["IP/32"]
+    cidr_blocks = ["103.137.19.231/32"]
+  }
+
+  # Egress rule to allow all outbound traffic
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1" # Allows all protocols
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
 resource "aws_instance" "mysql_server" {
-  ami           = "ami-0c55b159cbfafe1f0"  # Amazon Linux 2 AMI ID (change as needed)
-  instance_type = "t2.micro"
-  key_name      = "test"
+  ami                    = "ami-04b4f1a9cf54c11d0"
+  instance_type          = "t2.medium"
+  key_name               = "testing-key"
   vpc_security_group_ids = [aws_security_group.mysql_sg.id]
-  user_data = file("install_mysql.sh")
+  iam_instance_profile   = aws_iam_instance_profile.ssm_instance_profile.name
+  associate_public_ip_address = true
+}
+
+resource "aws_iam_role" "ssm_role" {
+  name = "SSMRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy_attachment" "ssm_managed_policy" {
+  name       = "SSMPolicyAttachment"
+  roles      = [aws_iam_role.ssm_role.name]
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+resource "aws_iam_instance_profile" "ssm_instance_profile" {
+  name = "SSMInstanceProfile"
+  role = aws_iam_role.ssm_role.name
 }
