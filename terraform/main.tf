bdaba903bdcf90778f4b813c4c3fd66903a16800
Umang <48994342+UMANG2707@users.noreply.github.com>
2025-03-10 20:25:27 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,26 @@
+resource "aws_security_group" "mysql_sg" {
+  name        = "mysql-security-group"
+  description = "Allow SSH and MySQL access"
 
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["IP/32"]
+  }
+
+  ingress {
+    from_port   = 3307
+    to_port     = 3307
+    protocol    = "tcp"
+    cidr_blocks = ["IP/32"]
+  }
+}
+
+resource "aws_instance" "mysql_server" {
+  ami           = "ami-0c55b159cbfafe1f0"  # Amazon Linux 2 AMI ID (change as needed)
+  instance_type = "t2.micro"
+  key_name      = "test"
+  vpc_security_group_ids = [aws_security_group.mysql_sg.id]
+  user_data = file("install_mysql.sh")
+}
